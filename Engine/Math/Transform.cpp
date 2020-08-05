95d1f63ee5785bb4fea42781145abf54713334d1
Matthew Mason <matthewmason7@outlook.com>
2020-08-05 16:43:04 -0600

    Add Math Filter with items to Engine

---
+++ Engine/Math/Transform.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+#include "Transform.h"
+
+namespace nc {
+	std::istream& operator >> (std::istream stream, Transform& transform)
+	{
+		stream >> transform.position;
+		stream >> transform.scale;
+		stream >> transform.angle;
+
+		return stream;
+	}
+	void Transform::Update(){
+		nc::Matrix33 mxs;
+		mxs.Scale(scale);
+		
+		nc::Matrix33 mxr;
+		mxr.Rotate(angle);
+		
+		nc::Matrix33 mxt;
+		mxt.Translate(position);
+
+		matrix = mxs * mxr * mxt;
+	}
+	void Transform::Update(const Matrix33& mx) {
+		nc::Matrix33 mxs;
+		mxs.Scale(scale);
+
+		nc::Matrix33 mxr;
+		mxr.Rotate(angle);
+
+		nc::Matrix33 mxt;
+		mxt.Translate(position);
+
+		matrix = mxs * mxr * mxt * mx;
+	}
+}
+
--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <SDL.h>
 
-int main(int, char**)
-{
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
-	{
+int main(int, char**){
+	if (SDL_Init(SDL_INIT_VIDEO) != 0){
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
@@ -16,8 +14,68 @@ int main(int, char**)
 		return 1;
 	}
 
-	// wait for keyboard enter to exit
-	std::getchar();
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == nullptr) {
+		std::cout << "Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	//create texture
+	int width = 128;
+	int height = 128;
+
+	SDL_Texture* texture1 = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, width, height);
+	
+	Uint32* pixels = new Uint32[width * height];
+	memset(pixels, 128, width * height * sizeof(Uint32));
+	SDL_UpdateTexture(texture1, NULL, pixels, width * sizeof(Uint32));
+	
+	SDL_Surface* surface = SDL_LoadBMP("sf2.bmp");
+	if (surface == nullptr) {
+		return 1;
+	}
+	SDL_Texture* texture2 = SDL_CreateTextureFromSurface(renderer, surface);
+	
+
+	SDL_Event event;
+	bool quit = false;
+	while (!quit) {
+		SDL_PollEvent(&event);
+		switch (event.type) {
+		case SDL_QUIT:
+			quit = true;
+			break;
+		}
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderClear(renderer);
+
+		//draw
+		for (size_t i = 0; i < width * height; i++) {
+			Uint8 c = rand() % 256;
+			pixels[i] = (c << 24 | c << 16 | c << 8);
+		}
+		//pixel memory = (8/8/8/8), (8/8/8/8)
+		//pixel memory = (c/c/c/0), (c/c/c/0)
+		SDL_UpdateTexture(texture1, NULL, pixels, width * sizeof(Uint32));
+
+		SDL_Rect rect;
+		rect.x = 200;
+		rect.y = 200;
+		rect.w = width;
+		rect.h = height;
+		SDL_RenderCopy(renderer, texture1, NULL, &rect);
+
+		SDL_Rect rect2;
+		rect2.x = 20;
+		rect2.y = 20;
+		SDL_QueryTexture(texture2, NULL, NULL, &rect2.w, &rect2.h);
+
+		SDL_RenderCopy(renderer, texture2, NULL, &rect2);
+
+		SDL_RenderPresent(renderer);
+	}
 
 	SDL_Quit();
 
